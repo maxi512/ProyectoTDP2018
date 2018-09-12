4cd984ac93cb7c418fd5406b4a0d01c86cff250e
ROMINA <romivire@hotmail.com>
2018-09-12 09:58:46 -0300

    Modifique linea de GUI

--- GUI_test_basic/src/gui/GUI.java
+++ GUI_test_basic/src/gui/GUI.java
@@ -47,7 +47,7 @@ public class GUI extends JFrame {
 			}
 		});
 		getContentPane().setLayout(null);
-		
+		this.setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 400, 600);
 		contentPane = new JPanel();
